aca73228be0a3d154bc6ea68c4597ad7c01c8108
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-08-06 00:25:51 +0200

    Create SourceList.js

---
+++ src/Main/js/me/corebyte/EMBDR/Sources/SourceList.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        Name: "Youtube",
+        Domains: [
+            "www.youtube.com",
+            "youtube.com",
+            "youtu.be"
+        ],
+
+        PreProcess: function(Url) {
+            
+        },
+    }
+]
