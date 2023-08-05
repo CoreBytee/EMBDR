d931d385719637ffa430f3004f9bd6aa3fe6f296
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-08-06 00:26:12 +0200

    Update Main.js

--- src/Main/js/me/corebyte/EMBDR/Main.js
+++ src/Main/js/me/corebyte/EMBDR/Main.js
@@ -1,3 +1,69 @@
-console.log("Hello js world")
+console.log("Hello World!")
 
-Import("me.corebyte.EMBDR.Helper.ListLinks")("oei https://www.reddit.com/r/tokkiefeesboek/comments/15g20ri/ook_deze_spreekt_voor_zich/")
+const FS = require("fs-extra")
+const DiscordJs = require("discord.js")
+
+// module.exports = async function() {
+
+//     globalThis.LingBot = {
+//         Words: Import("me.corebyte.LingBot.Words"),
+//         Config: FS.readJSONSync("./Config.json"),
+//         Client: new DiscordJs.Client(
+//             {
+//                 intents: [3276799],
+//             }
+//         ),
+//         REST: new DiscordJs.REST({ version: "10" }),
+//         Commands: [
+//             Import("me.corebyte.LingBot.Commands.PingCommand"),
+//             Import("me.corebyte.LingBot.Commands.RevealCommand"),
+//         ],
+//         Emojis: {
+//             Blocks: {
+//                 Blue: ":blue_square:",
+//                 Brown: ":brown_square:",
+//                 Green: ":green_square:",
+//                 Orange: ":orange_square:",
+//                 Purple: ":purple_square:",
+//                 Red: ":red_square:",
+//                 White: ":white_large_square:",
+//                 Yellow: ":yellow_square:",
+//             }
+//         },
+//         WordsManager: Import("me.corebyte.LingBot.WordsManager")
+//     }
+//     LingBot.REST.setToken(LingBot.Config.Token)
+
+//     await LingBot.Client.login(LingBot.Config.Token);
+//     await Import("me.corebyte.LingBot.DeployCommands")()
+//     LingBot.Channel = await LingBot.Client.channels.fetch(LingBot.Config.ChannelId)
+//     LingBot.Guild = LingBot.Channel.guild
+//     await LingBot.WordsManager.LoadHandles()   
+//     await LingBot.WordsManager.NewWord()
+
+
+// }
+
+async function Main() {
+
+    globalThis.EMBDR = {
+        Config: FS.readJSONSync("./Config.json"),
+        Sources: Import("me.corebyte.EMBDR.Sources.SourceList"),
+        Client: new DiscordJs.Client(
+            {
+                intents: [3276799],
+            }
+        ),
+        DiscordRest: new DiscordJs.REST({ version: "10" }),
+
+        Interactions: Import("me.corebyte.EMBDR.Interactions.Main")
+    }
+
+    EMBDR.DiscordRest.setToken(EMBDR.Config.Token)
+    await EMBDR.Client.login(EMBDR.Config.Token)
+
+    await Import("me.corebyte.EMBDR.Helper.DeployInteractions")()
+
+}
+
+Main()
