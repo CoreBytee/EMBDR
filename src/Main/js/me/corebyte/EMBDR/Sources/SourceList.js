26374bb3948783e1d995fab6c38a46a9e1391198
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-11-05 15:48:39 +0100

    Update SourceList.js

--- src/Main/js/me/corebyte/EMBDR/Sources/SourceList.js
+++ src/Main/js/me/corebyte/EMBDR/Sources/SourceList.js
@@ -1,14 +1,28 @@
 module.exports = [
-    {
-        Name: "Youtube",
-        Domains: [
-            "www.youtube.com",
-            "youtube.com",
-            "youtu.be"
-        ],
+    // {
+    //     Name: "Youtube",
+    //     Domains: [
+    //         "www.youtube.com",
+    //         "youtube.com",
+    //         "youtu.be"
+    //     ],
 
-        PreProcess: function(Url) {
+    //     PreProcess: function(Url) {
             
+    //     },
+    // },
+
+    {
+        Name: "Reddit",
+        Domains: [
+            "www.reddit.com",
+            "reddit.com"
+        ],
+        PreProcess: function(Url) {
+            return Url
         },
+        IsBad: function(Url) {
+            return false
+        }
     }
 ]
