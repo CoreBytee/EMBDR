242994c732a21670f7cfd7d58ef91e629cabdb80
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-11-05 15:48:28 +0100

    Update YtdlpCommand.js

--- src/Main/js/me/corebyte/EMBDR/Helper/YtdlpCommand.js
+++ src/Main/js/me/corebyte/EMBDR/Helper/YtdlpCommand.js
@@ -3,12 +3,12 @@ const Path = require("path")
 
 const YtdlpPath = Path.join(
     process.cwd,
-    "./bin/",
+    "./Binaries/",
     TypeWriter.OS == "win32" ? "yt-dlp.exe" : "yt-dlp"
 )
 
 console.log(YtdlpPath)
 
 module.exports = function(Arguments=[]) {
-
+    console.log(ChildProcess.spawnSync(YtdlpPath))
 }
