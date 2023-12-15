662a1e85080766af2b327ca4b7579cd71de4a0cc
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-12-15 11:25:57 +0100

    request data

--- src/Main/js/me/corebyte/EMBDR/Sources/Tiktok/Main.js
+++ src/Main/js/me/corebyte/EMBDR/Sources/Tiktok/Main.js
@@ -1,8 +1,22 @@
 const GetVideoId = require('get-video-id')
+const TiktokDL = require("@tobyg74/tiktok-api-dl").TiktokDL
 
 return {
     Name: "Tiktok",
     Match: function(Url) {
+        return GetVideoId(Url.href).service == "tiktok"
+    },
+    ReplaceMessage: async function(Message, Parts) {
 
+    },
+    Embed: async function(Url) {
+        const RequestData = await TiktokDL(Url.href, { version: "v3" })
+        const VideoData = RequestData.result
+        console.log(VideoData)
+        return {
+            Title: VideoData.desc,
+            AuthorName: VideoData.author.nickname,
+            MediaUrl: VideoData.video2
+        }
     }
 }
