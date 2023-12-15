f7647588d851d888b6d810545f4d8ad3a79b07ef
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-12-15 15:00:05 +0100

    replace msg

--- src/Main/js/me/corebyte/EMBDR/Helper/TryEmbed.js
+++ src/Main/js/me/corebyte/EMBDR/Helper/TryEmbed.js
@@ -4,15 +4,32 @@
 const DiscordJs = require("discord.js")
 
 const ExtractUrls = await Import("me.corebyte.EMBDR.Helper.ExtractUrls")
+const UltraHook = await Import("me.corebyte.UltraHook")
 
-return async function(Options) {
+return async function (Options) {
     const Content = Options.Content
     const Channel = Options.Channel
-    const Message = Options.Message
+    let Message = Options.Message
 
     const Urls = ExtractUrls(Content)
 
     const Parts = Urls.GetParts()
+
+    if (Message) {
+        for (const Source of EMBDR.Sources) {
+            await Source.ReplaceParts(Parts)
+        }
+
+        let NewMessage = ""
+        for (const Part of Parts) {
+            NewMessage += (Part.Replace || Part.Content) + " "
+        }
+
+        if (NewMessage != Content) {
+            Message = await UltraHook.ReplaceMessage(Message, NewMessage)
+        }
+    }
+
     for (const Index in Parts) {
         const Part = Parts[Index]
         if (!Part.Url) { continue }
