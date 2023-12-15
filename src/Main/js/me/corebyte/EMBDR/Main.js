e1f8dc94e1a0f9ec4e56d1f9d187b671c4c24147
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-12-15 11:25:43 +0100

    clean

--- src/Main/js/me/corebyte/EMBDR/Main.js
+++ src/Main/js/me/corebyte/EMBDR/Main.js
@@ -10,17 +10,15 @@ globalThis.EMBDR = {}
 EMBDR.Config = FS.readJSONSync("./Config.json")
 EMBDR.Sources = await Import("me.corebyte.EMBDR.Sources")
 EMBDR.DiscordJs = {}
+EMBDR.DiscordJs.Rest = new DiscordJs.REST({ version: "10" })
 EMBDR.DiscordJs.Client = new DiscordJs.Client(
     {
         intents: [3276799],
     }
 )
-EMBDR.DiscordJs.Rest = new DiscordJs.REST({ version: "10" })
-// EMBDR.Interactions = await Import("me.corebyte.EMBDR.Interactions.Main")
 
 EMBDR.DiscordJs.Rest.setToken(EMBDR.Config.Token)
 await EMBDR.DiscordJs.Client.login(EMBDR.Config.Token)
-
-await DeployInteractions(Interactions, EMBDR.DiscordJs.Client, EMBDR.DiscordJs.Rest, EMBDR.Config.Dev ? EMBDR.Config.GuildId : undefined)
+DeployInteractions(Interactions, EMBDR.DiscordJs.Client, EMBDR.DiscordJs.Rest)
 
 await Import("me.corebyte.EMBDR.Events.MessageCreate")
