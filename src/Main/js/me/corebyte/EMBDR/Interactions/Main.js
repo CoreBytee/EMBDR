e4148bf82759c537896e5d7f675f611e37c5e4cf
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-12-15 14:59:50 +0100

    remove interactions

--- src/Main/js/me/corebyte/EMBDR/Interactions/Main.js
+++ src/Main/js/me/corebyte/EMBDR/Interactions/Main.js
@@ -1,7 +1,7 @@
 return [
     //Components
     //Message contexts
-    await Import("me.corebyte.EMBDR.Interactions.MessageContext.Embed"),
+    // await Import("me.corebyte.EMBDR.Interactions.MessageContext.Embed"),
     //Slash commands
     // Import("me.corebyte.EMBDR.Interactions.SlashCommands.Embed")
 ]
